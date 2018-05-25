f6fa4a7ca3f4230bfd8d6becf5036eded54aaf56
jaysonvelagio <31393796+jaysonvelagio@users.noreply.github.com>
2018-05-25 12:30:51 +0800

    Create installer.sh

---
+++ Centos/installer.sh
@@ -0,0 +1,48 @@
+#--------------------------------------------------
+#Softether auto install script
+#Centos 7
+#Created May 25, 2018
+#Latest Softether Server Version for May 2018
+#Coded by Jayson Velagio
+#Open Source Project From https://www.softether.org/
+#From university of Tsukuba, Japan
+#--------------------------------------------------
+#Login with root permission or execute #sudo su
+#Script start
+#--------------------------------------------------
+#
+#Updating centos version
+cd /root
+yum update -y
+yum groupinstall "development tools"
+#Downloading server files
+yum install wget -y
+wget http://www.softether-download.com/files/softether/v4.27-9666-beta-2018.04.21-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
+tar zxf softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
+cd vpnserver
+#Installing server files, Manual input
+make
+cd /root
+mv vpnserver /usr/local
+rm -rf softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
+cd /usr/local/vpnserver
+chmod 600 *
+chmod 700 vpncmd
+chmod 700 vpnserver
+#Installing server command
+wget https://raw.githubusercontent.com/jaysonvelagio/SoftetherAutoInstallMultiPlatform/master/Centos/vpn-server.sh --no-check-certificate
+mv vpn-server.sh /etc/init.d/vpnserver
+cd /etc/init.d/
+chmod 755 vpnserver
+chkconfig --add vpnserver
+/etc/init.d/vpnserver start
+cd /usr/local/vpnserver
+echo ---------------------------------------------
+echo "VPN Server installed succesfully"
+echo "vpncmd is at /usr/local/vpnserver"
+echo ---------------Commands----------------------
+echo /etc/init.d/vpnserver start - to start
+echo /etc/init.d/vpnserver restart - to restart
+echo /etc/init.d/vpnserver stop - to stop
+echo ---------------------------------------------
+#End of script
