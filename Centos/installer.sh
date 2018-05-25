e29c3153ee2d5cef739c1c585014f04794f6627d
jaysonvelagio <31393796+jaysonvelagio@users.noreply.github.com>
2018-05-25 14:03:25 +0800

    Update installer.sh

--- Centos/installer.sh
+++ Centos/installer.sh
@@ -34,6 +34,7 @@ mv vpn-server.sh /etc/init.d/vpnserver
 cd /etc/init.d/
 chmod 755 vpnserver
 chkconfig --add vpnserver
+clear
 /etc/init.d/vpnserver start
 cd /usr/local/vpnserver
 echo ---------------------------------------------
