dfbf03ff353e02cff1ce92cdb9b5f039dceeb412
jaysonvelagio <31393796+jaysonvelagio@users.noreply.github.com>
2018-05-27 21:33:36 +0800

    Update installer.sh

--- Centos/installer.sh
+++ Centos/installer.sh
@@ -16,7 +16,7 @@ yum update -y
 yum groupinstall "development tools" -y
 #Downloading server files
 yum install wget -y
-wget http://www.softether-download.com/files/softether/v4.27-9667-beta-2018.05.26-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
+wget http://www.softether-download.com/files/softether/v4.27-9666-beta-2018.04.21-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
 tar zxf softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
 cd vpnserver
 clear
