e831ad27328907ff6338ce5f1acf8cf69d07bb14
jaysonvelagio <31393796+jaysonvelagio@users.noreply.github.com>
2018-05-27 21:34:19 +0800

    Update installer.sh

--- Debian and Ubuntu/installer.sh
+++ Debian and Ubuntu/installer.sh
@@ -15,7 +15,7 @@ cd /root
 apt-get update -y
 apt-get install build-essential -y
 #Downloading server files
-wget http://www.softether-download.com/files/softether/v4.27-9667-beta-2018.05.26-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
+wget http://www.softether-download.com/files/softether/v4.27-9666-beta-2018.04.21-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
 tar zxf softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
 cd vpnserver
 #Installing server files, Manual input
