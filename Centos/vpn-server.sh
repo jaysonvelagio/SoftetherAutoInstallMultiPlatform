fe7b083dbca73e63bab9b120f88e44bf646275a0
jaysonvelagio <31393796+jaysonvelagio@users.noreply.github.com>
2018-06-20 03:03:03 +0800

    Update vpn-server.sh

--- Centos/vpn-server.sh
+++ Centos/vpn-server.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 ### BEGIN INIT INFO
-# Provides:          vpnserver
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
+# Provides: vpnserver
+# Required-Start: $remote_fs $syslog
+# Required-Stop: $remote_fs $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
 # Short-Description: Start daemon at boot time
-# Description:       Enable Softether by daemon.
+# Description: Enable Softether by daemon.
 ### END INIT INFO
 DAEMON=/usr/local/vpnserver/vpnserver
 LOCK=/var/lock/subsys/vpnserver
