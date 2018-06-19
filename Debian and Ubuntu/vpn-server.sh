b94384bd85d3ce857604547d00ee42cc7697a5a8
jaysonvelagio <31393796+jaysonvelagio@users.noreply.github.com>
2018-06-20 03:03:23 +0800

    Update vpn-server.sh

--- Debian and Ubuntu/vpn-server.sh
+++ Debian and Ubuntu/vpn-server.sh
@@ -1,24 +1,13 @@
 #!/bin/sh
-### BEGIN INIT INFO
-# Provides:          vpnserver
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Start daemon at boot time
-# Description:       Enable Softether by daemon.
-### END INIT INFO
+# chkconfig: 2345 99 01
+# description: SoftEther VPN Server
 DAEMON=/usr/local/vpnserver/vpnserver
 LOCK=/var/lock/subsys/vpnserver
-TAP_ADDR=192.168.7.1
-
 test -x $DAEMON || exit 0
 case "$1" in
 start)
 $DAEMON start
 touch $LOCK
-sleep 1
-/sbin/ifconfig tap_soft $TAP_ADDR
 ;;
 stop)
 $DAEMON stop
@@ -28,8 +17,6 @@ restart)
 $DAEMON stop
 sleep 3
 $DAEMON start
-sleep 1
-/sbin/ifconfig tap_soft $TAP_ADDR
 ;;
 *)
 echo "Usage: $0 {start|stop|restart}"
